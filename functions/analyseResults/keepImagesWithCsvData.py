cb351b6b82f83ffa1a11ba0b0a550b7766a2b952
oskarpiibar <oskar.piibar@gmail.com>
2025-08-08 16:41:54 +0300

    Removing images without data points and the other way around

--- functions/analyseResults/keepImagesWithCsvData.py
+++ functions/analyseResults/keepImagesWithCsvData.py
@@ -20,24 +20,3 @@ def compare_images_with_csv(csv_path, image_folder):
     print(f"Extra images (not in CSV)    : {len(extra_in_folder)}")
 
     return missing_in_folder, extra_in_folder
-
-#
-# csv_path = r"C:\Program Files (x86)\Steam\steamapps\common\assettocorsa\apps\python\image_capture\data\17_25_20_16_06_2025.csv"
-# image_folder = r"C:\Program Files (x86)\Steam\steamapps\common\assettocorsa\apps\python\image_capture\images_17_25_20_16_06_2025"
-#
-# df = pd.read_csv(csv_path)
-# valid_filenames = set(df["image_filename"].dropna().astype(str))
-#
-# all_images = [f for f in os.listdir(image_folder) if f.endswith(".png")]
-#
-# missing_in_folder = valid_filenames - set(all_images)
-# extra_in_folder = set(all_images) - valid_filenames
-#
-# print(f"Images expected by CSV       : {len(valid_filenames)}")
-# print(f"Images actually in folder    : {len(all_images)}")
-# print(f"Missing images (only in csv) : {len(missing_in_folder)}")
-# print(f"Extra images (not in CSV)    : {len(extra_in_folder)}")
-#
-# # Optionally:
-# # print("\nMissing from folder:", missing_in_folder)
-# # print("\nExtra in folder:", extra_in_folder)
